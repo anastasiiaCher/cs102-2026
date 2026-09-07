5bc9c3a6059af8fcca2e4aaf2824408604714fe4
nikdvoronov-ship-it <nik.d.voronov@gmail.com>
2026-09-07 13:19:32 +0300

    Update hello_world.py

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,15 @@
-def text():
+"""Лаб00"""
+
+def text(message="Hello, World!"):
+    """
+    Возвращает текст
+
+    :param message: текст
+    :return: текст
+    """
     return message
+
+
+if __name__ = "__main__":
+    print(message)
+    
