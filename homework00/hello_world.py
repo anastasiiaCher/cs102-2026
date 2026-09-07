1405872c940514dc4ff81e918b0c64312fdea351
BRUTALLOLOL <xbrutallololx@gmail.com>
2026-09-07 12:18:22 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,14 @@
-def text():
+"""Лаба 00"""
+
+
+def text(message: str = "message") -> str:
+    """Возвращает переданное текстовое сообщение.
+
+    :param message: Строка текста для вывода.
+    :return: Исходная строка текста.
+    """
     return message
+
+
+if __name__ == "__main__":
+    print(text())
