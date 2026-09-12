292d114622fb7185e2d0c017208771c6de930d82
mariabatasova <batasovaamelia@gmail.com>
2026-09-12 15:19:51 +0300

    исправили ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,5 +1,4 @@
 """Module providing a function printing python version."""
-def\
-        text():
+def text():
     """Function printing python version."""
-    return 'message'
+    return "message"
